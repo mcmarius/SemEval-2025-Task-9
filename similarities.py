9332e63e7cf0ffba90e23f92bcaeb0f79325d368
mcmarius <23401453+mcmarius@users.noreply.github.com>
2024-11-24 19:57:49 +0200

    Cleanup

--- similarities.py
+++ similarities.py
@@ -1,6 +1,7 @@
 import csv
 import string
 import os
+import random
 
 import numpy as np
 import sentence_transformers
@@ -14,11 +15,8 @@ from sklearn.metrics import f1_score, accuracy_score
 torch.manual_seed(0)
 torch.backends.cudnn.benchmark = False
 
-import random
 random.seed(0)
-
 np.random.seed(0)
-# MAX_DISTANCE = 10000
 
 def read_file(file_name):
     with open(file_name, newline='') as csvfile:
@@ -47,13 +45,12 @@ def compute_score(hazards_true, products_true, hazards_pred, products_pred):
   return (f1_hazards + f1_products) / 2.
 
 
-def process_entries(model, entries, gold_key, eval_key, have_labels=True, always_use_title=False):
+def process_entries(model, entries, gold_key, eval_key, have_labels=True):
     if not have_labels:
         with open(f'data/{gold_key}_labels.csv') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
             gold_categories = []
             label2id = {}
-            # [line.strip() for line in f.readlines()]
             for row in reader:
                 gold_categories.append(row[gold_key])
                 label2id[row[gold_key]] = row['id']
@@ -71,64 +68,26 @@ def process_entries(model, entries, gold_key, eval_key, have_labels=True, always
     id2label = {k: gold_categories[k] for k in range(len(gold_categories))}
     parsed_entries = []
     for entry in entries:
-        # if always_use_title:
-        #     parsed_entries.append(entry['title'].lower().replace('\n', ' ').strip(string.punctuation))
-        #     continue
         new_entry = entry[eval_key].lower().replace('\n', ' ').strip(string.punctuation).strip()
         if 'missing' in new_entry:
             new_entry = new_entry.replace('missing', '').strip(string.punctuation).strip()
             if new_entry == '':
                 new_entry = entry['title']
-        # else:
-        #     if always_use_title:
-        #         new_entry = f"{new_entry} {entry['title']}"
-        #     # print(f'result: {new_entry}')
         parsed_entries.append(new_entry)
     # return
     # extracted_entries = list(entry[eval_key] for entry in entries)
     extracted_entries = parsed_entries
     gold_label_embeddings = model.encode(gold_categories)
     extracted_label_embeddings = model.encode(extracted_entries)
-    cos_sim = nn.CosineSimilarity(dim=0)
-    cos_sim2 = nn.CosineSimilarity(dim=1)
+    cos_sim = nn.CosineSimilarity(dim=1)
     num_correct = 0
     num_wrong = 0
     preds = []
     golds = []
-    # pairs = sentence_transformers.util.pairwise_cos_sim(extracted_label_embeddings[0:5], gold_label_embeddings[0:5])
-    # matr = nn.functional.cosine_similarity(torch.tensor(extracted_label_embeddings).unsqueeze(0), torch.tensor(gold_label_embeddings).unsqueeze(1), dim=2)
-    # matr = nn.functional.cosine_similarity(torch.tensor(extracted_label_embeddings[0]).unsqueeze(0), torch.tensor(gold_label_embeddings), dim=1)
-    # matr = cos_sim2(torch.tensor(extracted_label_embeddings[0]).unsqueeze(0), torch.tensor(gold_label_embeddings))
-    # print(cos_sim(torch.tensor(extracted_label_embeddings[0]), torch.tensor(gold_label_embeddings[0])))
-    # print(cos_sim(torch.tensor(extracted_label_embeddings[0]), torch.tensor(gold_label_embeddings[1])))
-    # print(cos_sim(torch.tensor(extracted_label_embeddings[0]), torch.tensor(gold_label_embeddings[2])))
-    # print(cos_sim(torch.tensor(extracted_label_embeddings[0]), torch.tensor(gold_label_embeddings[3])))
-    # print(gold_label_embeddings.shape)
-    # print(matr)
-    # sims, sim_ind = torch.max(matr, dim=0)
-    # print(sims)
-    # print(sim_ind)
-    # return
-    # for i in range(len(extracted_label_embeddings)):
-    #     # for j in range(len(gold_label_embeddings)):
-    #     if id2label[int(sim_ind[i])] == entries[i][gold_key]:
-    #         num_correct += 1
-    #     else:
-    #         num_wrong += 1
-    #     preds.append(int(sim_ind[i]))
-    #     golds.append(label2id[entries[i][gold_key]])
-    # print(torch.max(matr, dim=1))
-    # print(matr.shape)
-    # print(id2label[int(sim_ind)])
-    # print(torch.Tensor(extracted_label_embeddings[0:5]))
-    # print(torch.Tensor(gold_label_embeddings[0:5]))
-    # print(torch.Tensor(extracted_label_embeddings[0:5]).unsqueeze(0).shape)
-    # print(torch.Tensor(gold_label_embeddings[0:5]).unsqueeze(1).shape)
-    # return
     for i, extracted_entry in enumerate(tqdm.tqdm(extracted_label_embeddings)):
         max_similarity = 0
         predicted_label = None
-        matr = cos_sim2(torch.tensor(extracted_entry).unsqueeze(0), torch.tensor(gold_label_embeddings))
+        matr = cos_sim(torch.tensor(extracted_entry).unsqueeze(0), torch.tensor(gold_label_embeddings))
         sims, sim_ind = torch.max(matr, dim=0)
         if have_labels:
             if id2label[int(sim_ind)] == entries[i][gold_key]:
@@ -136,18 +95,6 @@ def process_entries(model, entries, gold_key, eval_key, have_labels=True, always
             else:
                 num_wrong += 1
         preds.append(int(sim_ind))
-        # for j, gold_entry in enumerate(gold_label_embeddings):
-        #     similarity = cos_sim( torch.tensor(extracted_entry), torch.tensor(gold_entry))
-        #     # print(f'comparing {extracted_entries[i]} with {gold_categories[j]}: got {distance}')
-        #     if similarity > max_similarity:
-        #         max_similarity = similarity
-        #         predicted_label = gold_categories[j]
-        # print(f'got predicted label {predicted_label}, expected {entries[i][gold_key]}')
-        # if predicted_label == entries[i][gold_key]:
-        #     num_correct += 1
-        # else:
-        #     num_wrong += 1
-        # preds.append(label2id[predicted_label])
         if have_labels:
             golds.append(label2id[entries[i][gold_key]])
         else:
@@ -248,8 +195,7 @@ def main(have_gold_labels=True):
                 writer.writerow({
                     'product-gold': product_gold[i], 'product-pred': product_pred[i],
                     'product-gold-text': id2label[product_gold[i]], 'product-pred-text': id2label[product_pred[i]]})
-    # hazard_pred, hazard_gold = process_entries(model, entries, 'hazard', 'title')
-    # product_pred, product_gold = process_entries(model, entries, 'product', 'title')
+
     submission_file_name = hazard_file_name
     if submission_file_name != product_file_name:
         submission_file_name = submission_file_name.split('.')[0] + '_' + product_file_name
@@ -270,26 +216,6 @@ def main(have_gold_labels=True):
         print(f'acc prod: {acc}')
         with open(f'results/{model_name2}/score_{hazard_file_name.split(".")[0]}.txt', 'w') as f:
             f.write(f'f1: {f1}')
-    # print(hazard_gold)
-    # print(product_gold)
-    # print(hazard_pred)
-    # print(product_pred)
-    # process_entries(model, entries, 'hazard', 'extracted_hazard')
-    # process_entries(model, entries, 'hazard-category', 'title')
-
-    # hazard_categories = set(entry['hazard-category'] for entry in entries)
-    # product_categories = set(entry['product-category'] for entry in entries)
-    # for entry in entries:
-    #     if 'missing' in entry['extracted_hazard'].lower():
-    #         entry['extracted_hazard'] = 'missing' + entry['extracted_hazard'].replace('missing', '')
-    # extracted_hazards = set(entry['extracted_hazard'] for entry in entries)
-    # extracted_products = set(entry['extracted_food'] for entry in entries)
-    # hazard_label_embeddings = model.encode(hazard_categories)
-    # product_label_embeddings = model.encode(product_categories)
-    # extracted_hazard_embeddings = model.encode(extracted_hazards)
-    # extracted_product_embeddings = model.encode(extracted_products)
-    # for hazard in extracted_hazard_embeddings:
-
 
 if __name__ == '__main__':
     main()
